2200d80c7162c0ef37684ac6dd91d0afab31ef06
root <youstinaobotrou@gmail.com>
2023-08-14 03:52:47 +0300

    task100

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -2,10 +2,10 @@
 
 /**
  *  * main - Entry point
- *
- *  * Return: Always 0 (Success)
- *   */
- int main(void)
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
 	    return (0);
 }
