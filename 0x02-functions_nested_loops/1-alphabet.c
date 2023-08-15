8f3d6a0df8927f33ece7bd31db7f313ff2b19eac
root <youstinaobotrou@gmail.com>
2023-08-15 15:16:17 +0300

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)	
+{
+	char x;
+	for(x='a';x<='z';x++)
+	{	
+		putchar(x);
+	}	
+	printf("\n");
+}
