72e39abaedb68b5e769dda7afa7b082ba0106e44
root <youstinaobotrou@gmail.com>
2023-08-15 14:19:15 +0300

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
+ *  * main - prints the alphabet in lowercase,
+ *   * followed by a new line
  *    * Return: Always 0 (Success)
  *     */
 int main(void)
 {
 	printf("_putchar\n");
-	    return (0);
+	return (0);
 }
