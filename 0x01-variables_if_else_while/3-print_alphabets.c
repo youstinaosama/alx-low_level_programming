fb4c8a9e34f45f20105d47b23c62fd4423598174
root <youstinaobotrou@gmail.com>
2023-08-16 00:09:15 +0300

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*  *  *  * main - Entry point
+*   *   *   *
+*    *    *    * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+	char x;
+	for(i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for(x = 'A' ; x <= 'Z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return(0);
+}	
+			
+
+
