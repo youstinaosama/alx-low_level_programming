aa52a8a480bf9264f33baadcd5198926a230d581
root <youstinaobotrou@gmail.com>
2023-08-15 15:20:29 +0300

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,13 @@
 /*
 * main - Entry point
 * Return: Always 0 (Success)
+* print_alphabet - entry point * * Description: This function prints the alphabet, in lowercase
 */
 void print_alphabet(void)	
 {
 	char x;
-	for(x='a';x<='z';x++)
+
+	for (x= 'a' ; x <= 'z' ; x++)
 	{	
 		putchar(x);
 	}	
