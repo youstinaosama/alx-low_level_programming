559a2eb1fa5524239428f66f5d52c14f441a5ee9
root <youstinaobotrou@gmail.com>
2023-08-16 18:00:56 +0300

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /*
-*_
+*_islower : Checks the small alphapet_
 @c: character to compare
 *Return: 1 if true, 0 if false
 */
 int _islower(int c)
 {
-   if (c >= 97 && c <= 127)
+	if (c >= 97 && c <= 127)
 	   return (1);
    return (0);
 }
