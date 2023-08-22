751bb525edf3fbeee41916e69659e8f13048c21f
root <youstinaobotrou@gmail.com>
2023-08-22 16:43:16 +0300

    task6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
-int main() 
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 1 (Success)
+ */
+int main(void) 
 { 
-	 system(“echo 'and that piece of art is useful"\ \- Dora Korpar, 2015-10-19'\n”); 
-	  return 0; 
+	write
+		(2,"and that piece of art is useful\" - Dora kopar, 2015-10-19\n" ,
+		 59);	
+	return (1); 
 } 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int longertype;
 	char charType;
 
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(longertype));
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	 printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
 }
