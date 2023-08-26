5e53d135b34f360a1878e3035ea2438515b2849d
root <youstinaobotrou@gmail.com>
2023-08-26 15:46:48 +0300

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  * main - assigns a random number to int n everytime
- *   * it executes, and prints it
+ *main - assigns a random number to int n everytime
+ *it executes, and prints it
  *Return: Always 0 (Success)
  */
 int main(void)
