863ef460d0a8534d09685c67f50e1f38b598d5f4
root <youstinaobotrou@gmail.com>
2023-08-27 02:03:51 +0300

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * int main (void) - check the code
+ * @a : number to multiply
+ *
+ * return : always 0
+ */
+/**
+ *main - check the code
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)	
+{
+	int mul;
+
+	mul = a*b; 
+	putchar(mul);
+	return (a*b);
+}
