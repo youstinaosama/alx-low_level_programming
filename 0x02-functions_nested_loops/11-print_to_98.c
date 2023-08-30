7c41d6637839ee7122ddb867e778a881ec5f2793
root <youstinaobotrou@gmail.com>
2023-08-31 02:14:04 +0300

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,28 +13,29 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d\t", i);
-			if (i == 98)
-				putchar('\n');
-			else
+			printf("%d", i);
+			if (i != 98)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			else
+				putchar('\n');
 		}
-	}
+	}	
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d\t", i);
-			if (i == 98)
-				putchar('\n');
-			else
+			if (i != 98)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			else
+				putchar('\n');
 		}
 	}
+	
 }
