b036442f204e0b897acafe117fd4b899122cc4b8
root <youstinaobotrou@gmail.com>
2023-08-30 23:42:27 +0300

    task8

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-/*
-*main - prints the alphabet in lowercase,
-*followed by a new line
-*Return: Always 0 (Success)
+/**
+*print_alphabet_x10- print alphabet small
+*
 */
 void print_alphabet_x10(void)
 {
-    char ch;
-    int i;	    ;
+	char ch;
+	int i;
 
-    for (i = 1 ; i <= 10 ; i++)
-    {
-         for (ch = 'a'; ch <= 'z'; ch++)
-         {
-	      putchar(ch);
-         }	   
-         putchar('\n');
-    }	 
-     
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer(void)- print every minute of day
+ *
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
 
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '3'; j++)
+		{
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(':');
+					putchar(k);
+					putchar(l);
+					putchar('\n');
+				}
+			}
+		}
+	}
+}
