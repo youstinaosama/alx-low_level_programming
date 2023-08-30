68a52d2b3de7222033425a4dad25d0712a3cafae
root <youstinaobotrou@gmail.com>
2023-08-30 18:07:00 +0300

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  *main - Entry point
  *Return: Always 1 (Success)
  */
-int main(void) 
-{ 
-	printf("and that piece of art is useful\"- Dora kopar, 2015-10-19\n");	
-	return (1); 
-} 
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
