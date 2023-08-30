6720d0da26b63dae8f87ef60fb085126417c1669
root <youstinaobotrou@gmail.com>
2023-08-30 18:03:52 +0300

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,6 @@
  */
 int main(void) 
 { 
-	write
-		(2,"and that piece of art is useful\" - Dora kopar, 2015-10-19\n" ,
-		 59);	
+	printf("and that piece of art is useful\"- Dora kopar, 2015-10-19\n");	
 	return (1); 
 } 
