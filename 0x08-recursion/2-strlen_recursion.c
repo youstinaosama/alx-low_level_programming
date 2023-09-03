ad42b2edb4e96b9a3c38e0ac403b0fc3cfc3ff0e
root <youstinaobotrou@gmail.com>
2023-09-03 17:18:10 +0300

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,12 @@
 
 /**
  *_strlen_recursion - print the length
- * @s: string 
+ *@s: string
  *    * Return: s+1.
  */
 int _strlen_recursion(char *s)
 {
-	if ( *s != '\0')
-		return (_strlen_recursion(s+1)+1);
+	if (*s != '\0')
+		return (_strlen_recursion(s + 1) + 1);
 	return (0);
 }
