0b263671ab11dfedaf6559400074399a9bbfb5da
root <youstinaobotrou@gmail.com>
2023-09-03 16:57:25 +0300

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
+ * _print_rev_recursion - prints rev of string
+ * @s: the string
+ *
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 
