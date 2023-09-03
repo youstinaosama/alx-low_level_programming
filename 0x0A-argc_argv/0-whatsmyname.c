09f6c5c640de4b1a977a5e84b2025da86217faca
root <youstinaobotrou@gmail.com>
2023-09-03 22:35:49 +0300

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: no of parameters
+ *@argv: array
+ *
+ *Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
