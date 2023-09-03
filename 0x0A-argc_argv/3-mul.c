8ee995a7e41ffdb90db7892b483fafb624a10f3f
root <youstinaobotrou@gmail.com>
2023-09-03 23:33:39 +0300

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,15 @@ int main(int argc, char **argv)
 	int mul = 1;
 
 	if (argc < 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 	{	
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 			mul = mul * atoi(argv[i]);
 	}
 	printf("%d\n", mul);
-	return (0);
+	return (1);
 }
