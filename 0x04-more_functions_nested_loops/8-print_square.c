0be8d67ec19644a6999b34d42b307109a8dc5323
Youstina osama <youstinaobotrou@gmail.com>
2023-10-07 21:02:22 +0300

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k < size; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
