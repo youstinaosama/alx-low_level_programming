90317d4fe6b183754f960a4aeb8a00d99802b9e4
Youstina osama <youstinaobotrou@gmail.com>
2023-10-07 22:01:18 +0300

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i%3 == 0) && (i%5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i == 100)
+			printf("\n");
+		printf(" ");
+	}
+	return (0);
+}
