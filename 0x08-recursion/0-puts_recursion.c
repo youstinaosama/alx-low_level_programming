d1e18b57b536f6e38e33ab75f1e96e4a9d45b595
Youstina osama <youstinaobotrou@gmail.com>
2023-10-25 18:48:48 +0300

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,20 +2,20 @@
 
 /**
  * _puts_recursion - print the string
- * @str: string to print
+ * @s: string to print
  *
  */
-void _puts_recursion(char *str)
+void _puts_recursion(char *s)
 {
-	if (*str != '\0')
+	if (*s != '\0')
 	{
-		_putchar(*str);
-		str++;
+		_putchar(*s);
+		s++;
 	}
 	else
 	{
 		_putchar('\n');
 		return;
 	}
-	_puts_recursion(str);
+	_puts_recursion(s);
 }
