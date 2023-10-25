da8ed774da1d32287eebf35209210076fc158658
Youstina osama <youstinaobotrou@gmail.com>
2023-10-25 23:30:48 +0300

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *is_prime_number - checks if the no. is prime
+ *
+ *@n: no. to check
+ *
+ * Return: always 0.
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+		return (0);
+	else if (n % 3 == 0)
+		return (0);
+	else if (n % 5 == 0)
+		return (0);
+	else if (n < 3)
+		return (0);
+	else 
+		return (1);
+}
