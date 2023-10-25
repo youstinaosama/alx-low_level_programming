2fc03feab05de85839df7f57248884492251a94f
Youstina osama <youstinaobotrou@gmail.com>
2023-10-25 21:25:12 +0300

    task0

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - reverse a string
+ *
+ *@s: the string
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_print_rev_recursion(s-1);
+		_putchar(*s);
+	}
+	else
+		_putchar(*s);
+	_putchar('\n');
+}
