4c286eb53ccb8bc7695e1c07a6dd9de09468d97b
Youstina osama <youstinaobotrou@gmail.com>
2023-10-26 01:12:12 +0300

    task5

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,14 +6,6 @@
  *
  * Return: always 0.
  */
-int is_palindrome(char *s)
+int is_palindrome(char *s, int i, int len)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	if (i <= 4)
-		return (0);
-	else
-		return (1);
-}
+	}
