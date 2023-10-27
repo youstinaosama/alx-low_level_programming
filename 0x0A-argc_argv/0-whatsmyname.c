90732f8218a50d2434d65125529a40b3a3019c9e
Youstina osama <youstinaobotrou@gmail.com>
2023-10-27 21:16:21 +0200

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints the name
+ *
+ *@argc: no. of arguments
+ *@argv: array of arguments
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
