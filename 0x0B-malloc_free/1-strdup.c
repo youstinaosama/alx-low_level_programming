14f5f9de507e5ed171143ad29bc2a4b554be8103
Youstina osama <youstinaobotrou@gmail.com>
2023-11-02 01:43:58 +0200

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,13 @@ char *_strdup(char *str)
 	int i, c;
 	char *arr;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	arr = (char *) malloc((i + 1) * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
 	for (c = 0; c < i; c++)
 		arr[c] = str[c];
 	return (arr);
