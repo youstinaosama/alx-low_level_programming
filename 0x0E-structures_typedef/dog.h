a597e0da8eca2be705c93bf04d9511f19b3caa85
Youstina osama <youstinaobotrou@gmail.com>
2023-11-06 20:17:46 +0200

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef dog_h
+#define dog_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *struct dog - a new type of data
+ * *
+ * *@name: name of dog
+ * *@age: age of dog
+ * *@owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
