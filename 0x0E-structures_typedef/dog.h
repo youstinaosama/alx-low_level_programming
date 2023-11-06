46d488b717b16b81a804bc77b16f70f47b4fcad8
Youstina osama <youstinaobotrou@gmail.com>
2023-11-06 20:34:02 +0200

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *  * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * *struct dog - a new type of data
  * *
