b33a4e08a58fe7b6adb9be5fbfdcc2c55ef275b2
Youstina osama <youstinaobotrou@gmail.com>
2023-11-09 20:55:53 +0200

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name - prints the name
+ *
+ *@name: string to print
+ *@f: function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
